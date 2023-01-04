1c6fc6416d3152dbd8ea4dd413b9b0f63ae047af
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-01-05 08:55:04 +0900

    first commit

---
+++ src/main/java/com/example/projectDae/ProjectDaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectDae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectDae/ProjectDaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectDae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
